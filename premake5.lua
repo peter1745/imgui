ac4f6d7a0e547f9162d38a39664293388153312d
Peter Nilsson <peter8nilsson@live.se>
2020-06-06 16:51:18 +0200

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "c++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+		
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
